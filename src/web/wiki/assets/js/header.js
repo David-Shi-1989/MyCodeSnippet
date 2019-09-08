f0f5f483f837aefcaaf27189c69a57e33744c32d
287841605@qq.com <Sww0829Wm0308>
2019-09-08 23:42:14 +0800

    header menu animate

---
+++ src/web/wiki/assets/js/header.js
@@ -0,0 +1,24 @@
+(function () {
+  window.header = {
+    def: {
+      id: 'header'
+    },
+    init: function () {
+      this.initEvent()
+    },
+    initEvent: function () {
+      this.bindMenuClick()
+    },
+    bindMenuClick () {
+      var me = this
+      $('#'+this.def.id+' .h-menu > li').on('click', function (evt) {
+        var $el = $(evt.target)
+        $('#'+me.def.id+' .h-menu > li.active').removeClass('active')
+        $el.addClass('active')
+        var left = ($el.index() * 2.5 + ($el.index() * 0.7))
+        $('#'+me.def.id+' .h-menu > li.active-bg').css('left', left+'rem')
+      })
+    }
+  }
+  header.init()
+})()
