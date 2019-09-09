7c12f7d1971ab41f69a51cb782b065c3fc8c9a99
David-Shi-1989 <287841605@qq.com>
2019-09-09 15:29:47 +0800

    完成顶部布局

--- src/web/wiki/assets/js/header.js
+++ src/web/wiki/assets/js/header.js
@@ -8,6 +8,7 @@
     },
     initEvent: function () {
       this.bindMenuClick()
+      this.bindLocaleItemClick()
     },
     bindMenuClick () {
       var me = this
@@ -17,6 +18,16 @@
         $el.addClass('active')
         var left = ($el.index() * 5 + ($el.index() * 1.4))
         $('#'+me.def.id+' .h-menu > li.active-bg').css('left', left+'rem')
+      })
+    },
+    bindLocaleItemClick () {
+      var me = this
+      $('#'+this.def.id+' .dropdown ul.dropdown-menu > li').on('click', function (evt) {
+        var $el = evt.target.tagName.toLowerCase() == 'li' ? $(evt.target) : $(evt.target.parentNode)
+        if (!$el.hasClass('active')) {
+          $el.addClass('active').siblings('.active').removeClass('active')
+          console.log('lang switch to ' + $el.text())
+        }
       })
     }
   }
